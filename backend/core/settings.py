59ee555d768865c59a959b6fe288fd453da78c6a
Jufis <andrey.marushkin.2017@mail.ru>
2025-11-25 23:42:32 +0300

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
-        conn_max_age=600
+        conn_max_age=600,
         engine='django.db.backends.postgresql'
     )
 }
